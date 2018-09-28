2981bba69bcd4ae691c4c6ced590d03302033a9b
carrillocarlosce <bestguru.peru@gmail.com>
2018-09-28 17:51:33 +0200

    configure webpack to build bundle.js

--- frontend/gymbosses/webpack.config.js
+++ frontend/gymbosses/webpack.config.js
@@ -1,30 +1,24 @@
 module.exports = {
-  entry: ['./src/index.js'],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015', 'stage-1']
-        }
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './',
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: 1000
+    "mode": "production",
+    "entry": "./src/index.js",
+    "output": {
+        "path": __dirname+'/static',
+        "filename": "bundle.js"
+    },
+    "module": {
+        "rules": [
+            {
+                "test": /\.js$/,
+                "exclude": /node_modules/,
+                "use": {
+                    "loader": "babel-loader",
+                    "options": {
+                        "presets": [
+                            "@babel/preset-react"
+                        ]
+                    }
+                }
+            }
+        ]
     }
-  }
 };
