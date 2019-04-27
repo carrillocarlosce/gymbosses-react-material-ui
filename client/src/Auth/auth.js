56f1ab149fae24d6767de1153f35cd447a95c968
carrillocarlosce <bestguru.peru@gmail.com>
2019-04-27 22:34:55 +0200

    change domain to www.gymbosses.com

--- client/src/Auth/auth.js
+++ client/src/Auth/auth.js
@@ -4,7 +4,7 @@ export default class Auth {
   auth0 = new auth0.WebAuth({
     domain: 'gymbosses.auth0.com',
     clientID: '6xwu89vXD1KZ592IJobbbZU1d2Wq5iUg',
-    redirectUri: 'http://domineering-degree.surge.sh/callback',
+    redirectUri: 'http://gymbosses.com/callback',
     responseType: 'token id_token',
     scope: 'openid profile'
   });
